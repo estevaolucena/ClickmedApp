c467fcd2846565d7d8f44e0e80caaefdb6203153
Estevao Lucena <estevao.llucena@gmail.com>
2019-11-06 14:02:16 -0200

    Criação do sidemenu e ajusstes de navegação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,24 +4,72 @@ import { BuscaProvider } from '../../providers/busca/busca';
 import { Medico } from '../../model/medico';
 import { ResultadoBuscaPage } from '../resultado-busca/resultado-busca';
 import { ToastProvider } from '../../providers/toast/toast';
+import { LoginPage } from '../../pages/login/login' 
+import { CadastroPage } from '../cadastro/cadastro';
+import { AuthProvider } from '../../providers/auth/auth';
 
+export interface PagesInterface {
+  title: String,
+  pageName: any,
+  icon: String
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   stringBusca: string;
   public medicos : Medico [];
-  
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
     private buscaProvider: BuscaProvider,
-    private toastProvider: ToastProvider) {
+    private toastProvider: ToastProvider,
+    private authProvider: AuthProvider) {
+  }
+
+  pages:  PagesInterface[]
+
+  menuItens(){
+    if (this.userLogged() == true) {
+      this.pages = [
+        {title: 'Sair', pageName: 'Logout', icon: 'log-out'}
+      ] 
+    } else {
+      this.pages = [
+        {title: 'Login', pageName: LoginPage, icon: 'log-in'},
+        {title: 'Cadastre-se', pageName: CadastroPage, icon: 'person-add'},
+      ]      
+    }
+  }
+
+  openPage(page){
+    if(page.pageName == 'Logout'){
+      this.logout()
+    }
+    let pageName = page.pageName
+    this.navCtrl.push(pageName)
+  }
+
+  ionViewCanEnter(){
+    this.menuItens()
+  }
+
+  userLogged(){
+    if(this.authProvider.getToken != null){
+      return true
+    }
+  }
+
+  logout(){
+  this.authProvider.logout()
+  this.navCtrl.push(HomePage)
   }
     
   buscaMedicos(){
-    if(this.stringBusca == null){
+    if(this.stringBusca == null || this.stringBusca == ''){
       this.toastProvider.exibirToast('Pesquise pelo nome, especialidade ou localização :)');
       return
     }
